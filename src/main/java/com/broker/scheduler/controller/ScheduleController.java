01a85e253ce417bc8e3aa4ebcd16260edce27cd6
William Ahrons <william.ahrons@gmail.com>
2017-06-01 22:09:54 -0300

    Create POST for schedule

--- src/main/java/com/broker/scheduler/controller/ScheduleController.java
+++ src/main/java/com/broker/scheduler/controller/ScheduleController.java
@@ -3,7 +3,9 @@ package com.broker.scheduler.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @RestController
 @RequestMapping(value = "/schedule")
@@ -15,8 +17,18 @@ public class ScheduleController {
         return new ScheduleDTO();
     }
 
+    @RequestMapping(method = POST,
+            consumes = APPLICATION_JSON_VALUE,
+            produces = APPLICATION_JSON_VALUE)
+    public SchedulerWrapper createSchedule() {
+        return new SchedulerWrapper();
+    }
+
     private class ScheduleDTO {
         private String id;
         private String manager;
+    }
+
+    private class SchedulerWrapper {
     }
 }
