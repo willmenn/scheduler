2955120ed8229073226a42e95962d91950c016e8
William Ahrons <william.ahrons@gmail.com>
2017-06-05 01:48:55 -0300

    Enable CORS

--- src/main/java/com/broker/scheduler/BrokerSchedulerApplication.java
+++ src/main/java/com/broker/scheduler/BrokerSchedulerApplication.java
@@ -3,10 +3,18 @@ package com.broker.scheduler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @EnableFeignClients
-public class BrokerSchedulerApplication {
+public class BrokerSchedulerApplication extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("PUT", "DELETE","POST","GET");
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BrokerSchedulerApplication.class, args);
