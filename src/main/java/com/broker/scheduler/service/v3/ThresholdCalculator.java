0ac468353e9da54ae7b1fe8d1b58879adc5e979e
William Ahrons <william.ahrons@gmail.com>
2018-05-08 01:37:33 -0300

    Change to use Standard Deviation instead of Average

--- src/main/java/com/broker/scheduler/service/v3/ThresholdCalculator.java
+++ src/main/java/com/broker/scheduler/service/v3/ThresholdCalculator.java
@@ -2,13 +2,23 @@ package com.broker.scheduler.service.v3;
 
 import com.broker.scheduler.service.v3.model.Schedule;
 
+
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class ThresholdCalculator {
 
-    public static Double average(List<Schedule.BrokerV3> brokerV3s){
+    public static Double average(List<Schedule.BrokerV3> brokerV3s) {
         return brokerV3s.stream()
                 .mapToInt(b -> b.getScore().intValue())
                 .summaryStatistics().getAverage();
+    }
+
+    public static Double standarDeviation(List<Schedule.BrokerV3> brokerV3s) {
+        int sum = brokerV3s.stream()
+                .mapToInt(b -> b.getScore().intValue()).sum();
+
+        double variance = sum / brokerV3s.size();
+        return Math.sqrt(variance);
     }
 }
